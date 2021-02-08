3a5ab6c19c8d0f57b61b463913522f5080177425
hsucode <terrence.hsu@outlook.com>
2021-02-08 21:59:56 +0800

    update

---
+++ V5R27/V5_INSTALLATION_HOTFIX.bat
@@ -0,0 +1,3 @@
+set MyPath=%~dp0
+"C:\Program Files\Dassault Systemes\B27\win_b64\code\bin\CATSoftwareMgtB" -hfxR
+%MyPath%HOTFIXR27SP6\CDROM\B27.6.39.win_b64\1\WIN64\StartHFXB -u "C:\Program Files\Dassault Systemes\B27" -killprocess  -v
