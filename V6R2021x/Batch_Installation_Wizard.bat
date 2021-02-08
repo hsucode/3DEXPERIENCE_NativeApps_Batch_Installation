380dfb9ec3b141d8ab25716673c16796d01e6321
hsucode <terrence.hsu@outlook.com>
2021-02-09 00:07:01 +0800

    add all

---
+++ V6R2021x/Batch_Installation_Wizard.bat
@@ -0,0 +1,28 @@
+
+@echo off
+
+::License
+@REM echo localhost:4085 >>C:\ProgramData\DassaultSystemes\Licenses\DSLicSrv.txt
+
+::Java
+@echo Oracle_Jre
+%~dp0Java\jre-8u281-windows-i586.exe /s
+
+echo 3DEXPERIENCE_GA
+%~dp0AM_3DEXP_NativeApps.AllOS\6\3DEXPERIENCE_NativeApps\1\StartTUI.exe --silent %~dp0UserIntentions\UserIntentions_CODE.xml
+
+echo 3DEXPERIENCE_FP
+%~dp0Part_3DEXP_NativeApps.Windows64\1\Software\HF_NativeApps_3DEXP.HF15.Windows64\1\StartTUI.exe --silent  %~dp0UserIntentions\UserIntentions_CODE_423.4.xml
+
+echo 3DEXPERIENCE_CAA
+%~dp0AM_3DEXP_AppDevSuite.AllOS\1\APIForNativeAppsDevelopment\Windows64\1\StartTUI.exe --silent  %~dp0UserIntentions\UserIntentions_CAA_423.4.xml
+
+echo 3DEXPERIENCE_CAA_FP
+%~dp0Part_3DEXP_AppDevSuite.AllOS\1\Software\HF_CAABuildTime.HF15.Windows64\1\StartTUI.exe --silent  %~dp0UserIntentions\UserIntentions_CAA_Additional_423.xml
+
+echo 3DEXPERIENCE_CAA_RADE
+%~dp0AM_3DEXP_AppDevSuite.AllOS\1\DevelopmentToolsetForNativeApps\Windows64\1\StartTUI.exe --silent  %~dp0UserIntentions\RADE\UserIntentions_CODE.xml
+
+echo 3DEXPERIENCE_CAA_RADE_FP
+%~dp0Part_3DEXP_AppDevSuite.AllOS\1\Software\DevelopmentToolsetForNativeApps\1\StartTUI.exe --silent  %~dp0UserIntentions\RADE\UserIntentions_CODE_423.4.xml
+
